618dc241069f1d240eb03ff9a6f4219bfbbaa98a
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-01-02 09:36:30 +0530

    create endpoint which will display studentname,university,clg

--- index.js
+++ index.js
@@ -2,4 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => res.send("HELLO"));
+
+app.get("/uid", (req, res) => res.send("55"));
+app.get("/clg", (req, res) => res.send("RKU"));
+
+app.get("/Sname", (req, res) => res.send("AT"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+//create endpoint which will display studentname,university,clg
