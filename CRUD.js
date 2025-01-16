48db7a4a7d7f7f6b64fd6319fb6f6a71f9378ab2
Ayushi Thumar <165537687+AThumar@users.noreply.github.com>
2025-01-16 09:21:57 +0530

    commit

--- CRUD.js
+++ CRUD.js
@@ -2,17 +2,32 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.json());
 
-var student = {
+var student =[ {
     UID: "2259",
     Name: "AT",
     Branch: "CE",
     Sem: 5,
     Div: "A",
     Rollno: 55
-}
+},
+];
 
 app.get("/", (req, res) => res.json(student));
+app.post("/", (req, res) => {
+    const {UID, Name, Branch, Sem, Div, Rollno} = req.body;
+    student = [...student,{UID, Name, Branch, Sem, Div, Rollno}];
+    res.json(student);
+});
+app.put("/:UID", (req, res) => {
+    const UID = req.params.UID;
+    const { Name, Branch, Sem, Div, Rollno} = req.body;
+    student = student.map((student) =>
+        
+         (student.UID === UID ? {UID, Name, Branch, Sem, Div, Rollno} : student)); 
+    res.json(student);    
+});
 app.listen(port, () =>
   console.log(`Server Started at http://localhost:${port}!`)
 );
